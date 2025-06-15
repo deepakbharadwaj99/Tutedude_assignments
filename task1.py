05a923295ef5757029e1fe3a1430f03e1181f304
deepakbharadwaj99 <103354576+deepakbharadwaj99@users.noreply.github.com>
2025-06-15 17:32:50 +0530

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,19 @@
+a= int(input("Enter the first number: "))
+b= int(input("Enter the second number: "))
+
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return  a-b
+def multi(a,b):
+    return a*b
+def div(a,b):
+    return a/b
+
+Addition= add(a,b)
+Substraction= sub(a,b)
+Multiplication= multi(a,b)
+Division= div(a,b)
+
+print("Addition: ",Addition ,"\nSubstraction: " ,Substraction,"\nMultiplication: ",Multiplication,"\nDivision: ",Division)
+
